64f3966925f880169f17b1f38767864be8c0c627
Андрій <andrey.maybrodskyy@gmail.com>
2023-04-21 00:23:31 +0300

    add repo docstring

--- src/services/auth.py
+++ src/services/auth.py
@@ -22,6 +22,14 @@ class Auth:
     redis_db = redis.Redis(host=settings.redis_host, port=settings.redis_port, db=0)
 
     def create_email_token(self, data: dict):
+        """
+        encode the email token
+
+        :param data: data to generate the email token
+        :type data: NoteModel
+        :return: token for email
+        :rtype: token
+        """
         to_encode = data.copy()
         expire = datetime.utcnow() + timedelta(days=7)
         to_encode.update({"iat": datetime.utcnow(), "exp": expire})
@@ -29,6 +37,14 @@ class Auth:
         return token
 
     async def get_email_from_token(self, token: str):
+        """
+        decode token for email
+
+        :param token: token to decode email
+        :type token: str
+        :return: email
+        :rtype: str
+        """
         try:
             payload = jwt.decode(token, self.SECRET_KEY, algorithms=[self.ALGORITHM])
             email = payload["sub"]
@@ -39,12 +55,40 @@ class Auth:
                                 detail="Invalid token for email verification")
 
     def get_hash(self, password: str):
+        """
+        password hash
+
+        :param password: password to hashstring
+        :type password: str
+        :return: hash
+        :rtype: str
+        """
         return self.pwd_context.hash(password)
 
     def verify_password(self, plain_password, password_hash):
+        """
+        password hash
+
+        :param plain_password: password
+        :type plain_password: str
+        :param password_hash: hashstring
+        :type password_hash: str
+        :return: verify result
+        :rtype: bool
+        """
         return self.pwd_context.verify(plain_password, password_hash)
 
     async def get_current_user(self, token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
+        """
+        get user by token
+
+        :param token: token to get user
+        :type token: token
+        :param db: current session to db
+        :type db: Session
+        :return: user
+        :rtype: bool
+        """
         credentials_exception = HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Could not validate credentials",
@@ -72,6 +116,16 @@ class Auth:
         return user
 
     async def create_access_token(self, data: dict, expires_delta: Optional[float] = None):
+        """
+        encode access token
+
+        :param data: data to be encoded
+        :type data: dict
+        :param expires_delta: second to life token
+        :type expires_delta: float
+        :return: token
+        :rtype: token
+        """
         to_encode = data.copy()
         if expires_delta:
             expire = datetime.utcnow() + timedelta(seconds=expires_delta)
@@ -82,6 +136,16 @@ class Auth:
         return encoded_access_token
 
     async def create_refresh_token(self, data: dict, expires_delta: Optional[float] = None):
+        """
+        encode refresh token
+
+        :param data: data to be encoded
+        :type data: dict
+        :param expires_delta: second to life token
+        :type expires_delta: float
+        :return: token
+        :rtype: token
+        """
         to_encode = data.copy()
         if expires_delta:
             expire = datetime.utcnow() + timedelta(seconds=expires_delta)
@@ -92,6 +156,14 @@ class Auth:
         return encoded_refresh_token
 
     async def decode_refresh_token(self, refresh_token: str):
+        """
+        decode refresh token
+
+        :param refresh_token: token to decode email
+        :type refresh_token: str
+        :return: email address
+        :rtype: str
+        """
         try:
             payload = jwt.decode(refresh_token, self.SECRET_KEY, algorithms=[self.ALGORITHM])
             if payload['scope'] == 'refresh_token':
