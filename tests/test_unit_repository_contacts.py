9c901b25f4fe91ca5bf3d690349b5bf2357f3e9f
Андрій <andrey.maybrodskyy@gmail.com>
2023-04-22 21:32:04 +0300

    add unit test for contacts

--- tests/test_unit_repository_contacts.py
+++ tests/test_unit_repository_contacts.py
@@ -4,7 +4,7 @@ from unittest.mock import MagicMock
 
 from sqlalchemy.orm import Session
 
-from src.database.models import Contact, Note, User
+from src.database.models import User, Contact
 from src.schemas import ContactModel
 from src.repository.contacts import (
     create,
@@ -13,7 +13,6 @@ from src.repository.contacts import (
     update,
     delete,
     find_by_name,
-    find_by_lastname,
     find_by_email,
     find_by_lastname,
     find_birthday7day,
@@ -35,17 +34,90 @@ class TestContacts(unittest.IsolatedAsyncioTestCase):
         self.assertEqual(result.last_name, body.last_name)
         self.assertEqual(result.email, body.email)
         self.assertTrue(hasattr(result, "id"))
-"""
-    get_all,
-    get_one,
-    update,
-    delete,
-    find_by_name,
-    find_by_lastname,
-    find_by_email,
-    find_by_lastname,
-    find_birthday7day,
-"""
+
+    async def test_get_all(self):
+        contacts = [Contact(), Contact()]
+        self.session.query().filter().offset().limit().all.return_value = contacts
+        result = await get_all(skip=1, limit=2, user=self.user, db=self.session)
+        self.assertEqual(result, contacts)
+
+    async def test_get_one_found(self):
+        contact = Contact()
+        self.session.query().filter().all.return_value = contact
+        result = await get_one(contact_id=1, user=self.user, db=self.session)
+        self.assertEqual(result, contact)
+
+    async def test_get_one_not_found(self):
+        self.session.query().filter().all.return_value = None
+        result = await get_one(contact_id=1, user=self.user, db=self.session)
+        self.assertIsNone(result)
+
+    async def test_update_found(self):
+        contact = Contact()
+        body = ContactModel(first_name="test", last_name="last", email="test@email.com", phone="234-345-2343",
+                            birthday=datetime.datetime.now())
+        self.session.query().filter().first.return_value = contact
+        self.session.commit.return_value = None
+        result = await update(contact_id=1, body=body, user=self.user, db=self.session)
+        self.assertEqual(result, contact)
+
+    async def test_update_not_found(self):
+        body = ContactModel(first_name="test", last_name="last", email="test@email.com", phone="234-345-2343",
+                            birthday=datetime.datetime.now())
+        self.session.query().filter().first.return_value = None
+        self.session.commit.return_value = None
+        result = await update(contact_id=1, body=body, user=self.user, db=self.session)
+        self.assertIsNone(result)
+
+    async def test_delete_found(self):
+        contact = Contact()
+        self.session.query().filter().all.return_value = contact
+        result = await delete(contact_id=1, user=self.user, db=self.session)
+        self.assertEqual(result, contact)
+
+    async def test_delete_not_found(self):
+        self.session.query().filter().all.return_value = None
+        result = await delete(contact_id=1, user=self.user, db=self.session)
+        self.assertIsNone(result)
+
+    async def test_find_by_name_found(self):
+        contact = Contact()
+        self.session.query().filter().all.return_value = contact
+        result = await find_by_name(contact_name='test', user=self.user, db=self.session)
+        self.assertEqual(result, contact)
+
+    async def test_find_by_name_not_found(self):
+        self.session.query().filter().all.return_value = None
+        result = await find_by_name(contact_name='test', user=self.user, db=self.session)
+        self.assertIsNone(result)
+
+    async def test_find_by_lastname_found(self):
+        contact = Contact()
+        self.session.query().filter().all.return_value = contact
+        result = await find_by_lastname(lastname='test', user=self.user, db=self.session)
+        self.assertEqual(result, contact)
+
+    async def test_find_by_lastname_not_found(self):
+        self.session.query().filter().all.return_value = None
+        result = await find_by_lastname(lastname='test', user=self.user, db=self.session)
+        self.assertIsNone(result)
+
+    async def test_find_by_email_found(self):
+        contact = Contact()
+        self.session.query().filter().all.return_value = contact
+        result = await find_by_email(email='test@email.com', user=self.user, db=self.session)
+        self.assertEqual(result, contact)
+
+    async def test_find_by_email_not_found(self):
+        self.session.query().filter().all.return_value = None
+        result = await find_by_email(email='test@email.com', user=self.user, db=self.session)
+        self.assertIsNone(result)
+
+    async def test_find_birthday7day(self):
+        contacts = [Contact(), Contact()]
+        self.session.query().filter().all.return_value = contacts
+        result = await find_birthday7day(user=self.user, db=self.session)
+        self.assertEqual(result, contacts)
 
 
 if __name__ == '__main__':
